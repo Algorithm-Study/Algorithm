493046528038538b339a9fbcc64a0ae27ef6d0f4
SangJunni <yoonsj0322@naver.com>
2023-02-23 16:54:31 +0900

    [Solved] 230323 윤상준 문제풀이

---
+++ greedy/P12980_윤상준.py
@@ -0,0 +1,23 @@
+## 효율성 테스트 4, 9 에서 실패 -> 다시 해보니 됨;;
+def solution(n):
+    ans = 0
+    while n > 1: #조건문으로 인해 실패 != 으로 바꾸면 성공
+        if n % 2 == 0:
+            n /= 2
+        else:
+            n -= 1
+            ans += 1
+
+    return ans + 1
+
+## 새로운 접근방식
+def solution(n):
+    ans = 0
+    while True:
+        goal = (n //2)*2
+        ans += (n-goal)
+        n = goal
+        if n < 2:
+            break
+        n //= 2
+    return ans + n
