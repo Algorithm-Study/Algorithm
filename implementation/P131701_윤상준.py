89ecc81b90f438196575bd4e9b488d524371481d
SangJunni <yoonsj0322@gmail.com>
2023-04-18 19:05:21 +0900

    [Solved] P131701 코드 개선

--- implementation/P131701_윤상준.py
+++ implementation/P131701_윤상준.py
@@ -6,3 +6,16 @@ def solution(elements):
         for j in range(len(elements)):
             cases.add(sum(choices[j:j+i]))
     return len(cases)
+
+#Refactoring -> 테스트20 4225.22ms -> 263.60ms
+def solution(elements):
+    choices = elements + elements
+    # 길이 1인 경우 미리 포함
+    cases = set(elements)
+    for i in range(1,len(elements)+1):
+        total = sum(choices[0:0+i])
+        cases.add(total)
+        for j in range(1,len(elements)):
+            total = total - choices[j-1] + choices[j+i-1]
+            cases.add(total)
+    return len(cases)
