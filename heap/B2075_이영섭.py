95a3b0f0acc08a4da0d5037cc0fae21eabc82de6
0seob <changas9263@gmail.com>
2023-05-19 04:33:00 +0900

    [Solved] 230518 이영섭 문제풀이

--- heap/B2075_이영섭.py
+++ heap/B2075_이영섭.py
@@ -2,10 +2,14 @@ import heapq
 
 N = int(input())
 data = []
-for _ in range(N):
-    num = map(int, input().split())
-    for n in num:
-        heapq.heappush(data, -n)
-for _ in range(N):
-    ans = heapq.heappop(data)
-print(-ans)
+for i in range(N):
+    num = list(map(int, input().split()))
+    if i == 0:
+        for n in num:
+            heapq.heappush(data, n)
+    else:
+        for n in num:
+            heapq.heappush(data, n)
+            heapq.heappop(data)
+
+print(heapq.heappop(data))
