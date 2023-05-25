ae72ffca327d69da180d9fbc00932aeaee7242d0
SangJunni <yoonsj0322@gmail.com>
2023-05-25 20:12:50 +0900

    [Solved] 230525 윤상준 문제 풀이

---
+++ implementation/B60058_윤상준.py
@@ -0,0 +1,40 @@
+def balanced_index(p):
+    count = 0
+    for i in range(len(p)):
+        if p[i] == '(':
+            count += 1
+        else:
+            count -= 1
+        if count == 0:
+            return i
+def check_proper(p):
+    count = 0
+    for i in p:
+        if i =='(':
+            count += 1
+        else:
+            if count == 0:
+                return False
+            count -= 1
+    return True
+
+def solution(p):
+    answer = ''
+    if p == '':
+        return answer
+    index = balanced_index(p)
+    u = p[:index+1]
+    v = p[index+1:]
+    if check_proper(u):
+        answer = u + solution(v)
+    else:
+        answer = '('
+        answer += solution(v)
+        answer += ')'
+        u = list(u[1:-1])
+        for i in range(len(u)):
+            if u[i] == '(':
+                answer += ')'
+            else:
+                answer += '('
+    return answer
