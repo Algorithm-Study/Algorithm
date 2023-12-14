7124c757413d4d2e99cd0bfe3eb7866951febcb5
Sungwoo Lee <leesw1217@naver.com>
2023-12-14 22:56:47 +0900

    [Fix] B2590_이성우

--- greedy/B2590_이성우.py
+++ greedy/B2590_이성우.py
@@ -2,25 +2,28 @@ import math
 tile = [int(input()) for _ in range(6)]
 answer = tile[3] + tile[4] + tile[5]
 answer += math.ceil(tile[2]/4)
-three = tile[2]%4
-two = tile[3]*5
-one = tile[4]*11
-if three == 1:
-    two += 5
-    one += 7
-elif three == 2:
-    two += 3
-    one += 6
-elif three == 3:
-    two += 1
-    one += 5
-if tile[1] > two:
-    answer += math.ceil((tile[1]-two)/9)
-    one += (36 - ((tile[1]-two)%9)*4)
-else:
-    one += (two-tile[1])*4
+empty_1x1 = tile[4]*11
+empty_2x2 = tile[3]*5
+empty_3x3 = tile[2]%4
+
+if empty_3x3 == 1:
+    empty_2x2 += 5
+    empty_1x1 += 7
+elif empty_3x3 == 2:
+    empty_2x2 += 3
+    empty_1x1 += 6
+elif empty_3x3 == 3:
+    empty_2x2 += 1
+    empty_1x1 += 5
     
-if tile[0] > one:
-    answer += math.ceil((tile[0]-one)/36)
+if tile[1] > empty_2x2:
+    answer += math.ceil((tile[1]-empty_2x2)/9)
+    if (tile[1]-empty_2x2)%9:
+        empty_1x1 += 36 - ((tile[1]-empty_2x2)%9)*4
+else:
+    empty_1x1 += (empty_2x2-tile[1])*4
+    
+if tile[0] > empty_1x1:
+    answer += math.ceil((tile[0]-empty_1x1)/36)
 
 print(answer)
